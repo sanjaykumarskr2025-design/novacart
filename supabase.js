0317d71c698781212871c9a394ec05a1e8c6243c
SANJAY <sanjaykumarskr2025@gmail.com>
2026-08-25 22:47:26 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // Paste your Supabase project URL and anon/publishable key here.
 // Supabase Dashboard → Project Settings → API.
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-const SUPABASE_URL="PASTE_YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY="PASTE_YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL="https://sanjaykumarskr2025-design.github.io/novacart/";
+const SUPABASE_ANON_KEY="sb_publishable_X6eH8X960UlOhLSqTyvdog_MF9xAJ63";
 export const supabase=createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
